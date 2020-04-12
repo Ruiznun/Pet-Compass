29ff8d5449e7cdf768327a02ca5db99ec204f3e4
Evan Vetrano <evanv1998@gmail.com>
2020-04-12 14:47:09 -0500

    created base app with menu bar (no working functions yet)

---
+++ src/Components/Headline.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+// import searchIcon from '../search-icon.png';
+
+class Headline extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            showForm: false
+        };
+    }
+
+    showForm() {
+        this.setState({
+            showForm: !this.state.showForm
+        });
+    }
+
+    render() {
+        let searchForm = this.state.showForm ? (
+            <form className="menu__search-form" method="POST">
+                <input className="menu__search-input" placeholder="Type and hit enter" />
+            </form>
+        ) : '';
+
+        return (
+            <nav className="menu">
+                <h1 className="menu__logo">Pet Compass</h1>
+
+                <div className="menu__right">
+                    <button onClick={this.showForm.bind(this)} className="menu__search-button">Filter</button>
+                    <button className="menu__list-button"></button>
+                    <button className="menu__block-button"></button>
+
+                    {searchForm}
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Headline;
