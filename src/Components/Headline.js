b76cdc6ab49407e15d83aa4281af30c92bf68495
Evan Vetrano <evanv1998@gmail.com>
2020-04-22 18:44:00 -0500

    removed headline links since they will be added ot the animal container

--- src/Components/Headline.js
+++ src/Components/Headline.js
@@ -4,36 +4,36 @@ class Headline extends Component {
     constructor() {
         super();
 
-        this.state = {
-            showForm: false
-        };
+        // this.state = {
+        //     showForm: false
+        // };
     }
 
-    showForm() {
-        this.setState({
-            showForm: !this.state.showForm
-        });
-    }
+    // showForm() {
+    //     this.setState({
+    //         showForm: !this.state.showForm
+    //     });
+    // }
 
     render() {
-        let searchForm = this.state.showForm ? (
-            <form className="menu__search-form" method="POST">
-                <input className="menu__search-input" placeholder="Type and hit enter" />
-            </form>
-        ) : '';
+        // let searchForm = this.state.showForm ? (
+        //     <form className="menu__search-form" method="POST">
+        //         <input className="menu__search-input" placeholder="Type and hit enter" />
+        //     </form>
+        // ) : '';
 
         return (
             <div className="container center">
                 <nav className="menu">
                     <h1 className="menu__logo">Pet Compass</h1>
 
-                    <div className="menu__right">
+                    {/* <div className="menu__right">
                         <button onClick={this.showForm.bind(this)} className="menu__search-button">Filter</button>
                         <button className="menu__list-button"></button>
                         <button className="menu__block-button"></button>
 
                         {searchForm}
-                    </div>
+                    </div> */}
                 </nav>
             </div>
         );
