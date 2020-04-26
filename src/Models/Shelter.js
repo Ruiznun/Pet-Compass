f358aec28a255875c2a65547e7431ef79ffa9435
Skyler <swillkeough@gmail.com>
2020-04-26 03:00:53 -0500

    Added server, testing, and multiple new npm modules.

---
+++ src/Models/Shelter.js
@@ -0,0 +1,10 @@
+class Shelter {
+    constructor(id_shelter, name, address, type, contact, zipcode) {
+        this.id_shelter = id_shelter;
+        this.name = name;
+        this.address = address;
+        this.type = type;
+        this.contact = contact;
+        this.zipcode = zipcode;
+    }
+}
---
+++ src/db.js
@@ -0,0 +1,61 @@
+let mysql = require('mysql');
+/*
+class Db {
+    constructor() {
+        this.connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'ReduxHacker42!',
+            database: 'world'
+        });
+    }
+    connectToDb() {
+        this.connection.connect(function(err) {
+            if (err) {
+                return console.error('error: ' + err.message);
+            }
+            console.log('Connected to the MySQL server.');
+        })
+    }
+
+    disconnectFromDb() {
+        this.connection.end();
+    }
+
+    queryDb(queryText) {
+        this.connection.query(queryText, (err, rows) => {
+            if (err) throw err;
+
+            console.log('Data receieved');
+            console.log('rows');
+            return(rows);
+        })
+    }
+
+}
+*/
+async function queryDb(queryText) {
+   const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'ReduxHacker42!',
+    database: 'world'
+   });
+
+   console.log("Made it to querydb");
+
+   return (new Promise(function(resolve, reject) {
+    connection.query(queryText, (err, rows) => {
+        if (err) reject(err);
+        
+        //connection.end();
+        console.log('Data received');
+        resolve(rows);
+    })
+}));
+
+};
+
+module.exports = {
+    queryDb
+}
