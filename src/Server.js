d360bc2bf72c76a8c0682a283a88c1bc0868fdca
Skyler <swillkeough@gmail.com>
2020-05-07 23:08:28 -0500

    blahblah

--- src/Server.js
+++ src/Server.js
@@ -1,13 +1,29 @@
 var getAllAnimals = require('./Controllers/AnimalController').Animals;
 var getAnimalsByType = require('./Controllers/AnimalController').AnimalsByType;
+const https = require('https');
+const fs = require('fs');
+
+var key = fs.readFileSync('../../ssl/apache-selfsigned.key');
+var cert = fs.readFileSync('../../ssl/apache-selfsigned.crt');
+var options = {
+  key: key,
+  cert: cert
+};
+
+
 
 const express = require('express');
 
 const expressApp = express();
 
+var server = https.createServer(options, expressApp);
+
 const PORT = 8080;
+const HTTPSPORT = 8043;
 
 expressApp.listen(PORT, () => console.log('Express server currently listening on port ' + PORT));
+server.listen(HTTPSPORT, () => {console.log('HTTPS express starting on port : ' + HTTPSPORT)
+});
 
 const anyAnimals = async function (request, response) {
   var resultAnimals = await getAllAnimals();
@@ -16,6 +32,7 @@ const anyAnimals = async function (request, response) {
 }
 
 expressApp.get('/animals', anyAnimals);
+server.get('/animals', anyAnimals);
 
 
 expressApp.get('/animalsByType', async function(request, response) {
